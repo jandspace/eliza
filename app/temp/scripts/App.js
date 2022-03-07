4bdd96c45ae40a3ed316f8b0ed8ba55ef9ad82e5
Joungmin Shin <jandspace@gmail.com>
2022-03-07 09:57:55 +0000

    Favicon update

--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -164,9 +164,9 @@ $(function () {
   $(".article-group > .article").slice(0, 4).show();
   $("body").on("click touchstart", ".load-more", function (e) {
     e.preventDefault();
-    $(".article:hidden").slice(0, 4).slideDown();
-    if ($(".article:hidden").length == 0) {
-      $(".load-more").css("visibility", "hidden");
+    $(".article-group > .article:hidden").slice(0, 4).slideDown();
+    if ($(".article-group > .article:hidden").length == 0) {
+      $("#LoadMore").css("visibility", "hidden");
     }
   });
 });
