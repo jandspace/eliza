86778c5c9c8dc2e2063e1a4ee4c7c99d2da798e6
Joungmin Shin <jandspace@gmail.com>
2022-03-10 16:04:41 +0000

    prototype update

--- app/assets/scripts/modules/LoadMore.js
+++ app/assets/scripts/modules/LoadMore.js
@@ -3,12 +3,24 @@
 //
 
 $(document).ready(function () {
-  $(".article-group > .article").slice(0, 4).show();
+  $("#Article-Container .article-group > .article").slice(0, 15).show();
   $("#loadMore").on("click", function (e) {
     e.preventDefault();
-    $(".article-group > .article:hidden").slice(0, 4).slideDown();
-    if ($(".article-group > .article:hidden").length == 0) {
-      $(".load-more").text("No more article").addClass("no-article");
+    $("#Article-Container .article-group > .article:hidden").slice(0, 15).slideDown();
+    if ($("#Article-Container .article-group > .article:hidden").length == 0) {
+      $("#Article-Container .load-more").text("No more article").addClass("no-article");
+    }
+  });
+});
+
+//Related Article
+$(document).ready(function () {
+  $("#Realted-Article .article-group > .article").slice(0, 4).show();
+  $("#Realted-Article #loadMore").on("click", function (e) {
+    e.preventDefault();
+    $("#Realted-Article .article-group > .article:hidden").slice(0, 4).slideDown();
+    if ($("#Realted-Article .article-group > .article:hidden").length == 0) {
+      $("#Realted-Article .load-more").text("No more article").addClass("no-article");
     }
   });
 });
